05a41d6a1a2502dbe58c9a09d31f8bc264fd1424
Jonathan Marshall <jmarshall@never.you.mind>
2023-02-24 11:49:24 +1300

    update with figure for pubmlst ordering

--- code/pubmlst_allele_ordering.R
+++ code/pubmlst_allele_ordering.R
@@ -5,4 +5,22 @@ camp <- read.table("raw/BIGSdb_250368_0481013786_82844.txt", sep="\t", header=TR
 alleles <- camp |> filter(!is.na(CAMP0864)) |> select(id, source, CAMP0864) |>
   group_by(CAMP0864) |> count(source) |> ungroup()
 
-alleles |> mutate(case_when(source ))
+alleles |> mutate(source = case_when(str_detect(source, "chick") ~ "Chicken",
+                            str_detect(source, "beef") ~ "Cattle",
+                            str_detect(source, "cattle") ~ "Cattle",
+                            str_detect(source, "cow") ~ "Cattle",
+                            str_detect(source, "lamb") ~ "Sheep",
+                            str_detect(source, "sheep") ~ "Sheep",
+                            str_detect(source, "human") ~ "Human",
+                            str_detect(source, "water") ~ "Water",
+                            TRUE ~ "Other")) |>
+  group_by(CAMP0864, source) |>
+  summarise(count = sum(n)) |>
+  ungroup() |>
+  mutate(Allele = as.numeric(CAMP0864)) |>
+  group_by(Allele) |>
+  filter(source != "Human") |>
+  mutate(NumSource = n_distinct(source)) |>
+  summarise(Source = if_else(first(NumSource) == 1, first(source), "Multiple")) |>
+  filter(!Source %in% c("Other", "Multiple")) |>
+  write_csv("data/camp0864.csv")
